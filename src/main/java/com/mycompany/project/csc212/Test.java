8593a15575e2b0c37a9ae23a8523325765e49712
Alanoud-10 <444201111@student.ksu.edu.sa>
2025-04-25 01:13:58 +0300

    Update Test.java

--- src/main/java/com/mycompany/project/csc212/Test.java
+++ src/main/java/com/mycompany/project/csc212/Test.java
@@ -12,7 +12,7 @@ public class Test {
 public static void main(String[] args) {
     
 PhotoManager manager = new PhotoManager();
-
+ InvIndexPhotoManager invmanager = new InvIndexPhotoManager();
  // Create photo with tags 
 Photo photo1 = new Photo("hedgehog.jpg",toTagsLinkedList("animal, hedgehog, apple, grass, green"));
 Photo photo2 = new Photo("bear.jpg",toTagsLinkedList("animal, bear, cab, grass,wind"));
@@ -22,11 +22,18 @@ Photo photo3 = new Photo("orange-butterfly.jpg",toTagsLinkedList("insect,butterf
     manager.addPhoto(photo1);
     manager.addPhoto(photo2);
     manager.addPhoto(photo3);
+    
+ // Add photos to invmanager
+    invmanager.addPhoto(photo1);
+    invmanager.addPhoto(photo2);
+    invmanager.addPhoto(photo3);
+    
 
 // Create albums
-//Album album1 = new Album("Album1", "bear", manager);
-//Album album2 = new Album("Album2", "animal AND grass", manager);
-
+     Album album1 = new Album("Album1", "bear", manager, invmanager);
+     Album album2 = new Album("Album2", "animal AND grass", manager, invmanager);
+     Album album3 = new Album("Album3", "", manager, invmanager);
+        
 // Print paths and tags of a photo
 System.out.println("Get photo1 path and tags:");
 System.out.println("\nphoto1 path: " + photo1.getPath());
@@ -35,23 +42,59 @@ LinkedList<String> tags = photo1.getTags();
 displayTags(tags);
 
   
- // Delete one photo and test result
-System.out.println("\nDelete the photo ’bear.jpg’:");
-manager.deletePhoto("bear.jpg");
 
-// Print remaining photos
-  System.out.println("\nRemaining photos after deletion:");
-  showPhotoPaths(manager.getPhotos());
-  
-//You can get the list of tags of photo1 by calling photo1.getTags().
-//You can write a method that prints the list of tags of photo1.
+  // === Album 1 Details ===
+        System.out.println("\n\n[ Album 1 Info ]");
+        System.out.println("Name       : " + album1.getName());
+        System.out.println("Condition  : " + album1.getCondition());
+        LinkedList<Photo> photos = album1.getPhotos();
+        System.out.println(album1.getCondition());
+        System.out.println("Photos     :");
+        showPhotoPaths(photos);
+        System.out.printf("Comparisons made for condition \"%s\": %d\n", album1.getCondition(), album1.getNbComps());
 
-// Print album2 details
-//System.out.println("Get album2 name, condition, and photos:");
-//System.out.println("album2 name: " + album2.getName());
-//System.out.println("album2 condition: " + album2.getCondition());
-//You can get the list of photos in album2 by calling album2.getPhotos().
-//You can write a method that prints the list of photos in album2.
+     // === Album 2 Details ===
+        System.out.println("\n\n[ Album 2 Info ]");
+        System.out.println("Name       : " + album2.getName());
+        System.out.println("Condition  : " + album2.getCondition());
+        photos = album2.getPhotos();
+        System.out.println(album2.getCondition());
+        System.out.println("Photos     :");
+        showPhotoPaths(photos);
+        System.out.printf("Comparisons made for condition \"%s\": %d\n", album2.getCondition(), album2.getNbComps());
+      
+      // === Album 3 Details ===
+        System.out.println("\n\n[ Album 3 Info ]");
+        System.out.println("Name       : " + album3.getName());
+        System.out.println("Condition  : " + album3.getCondition());
+        photos = album3.getPhotos();
+        System.out.println(album3.getCondition());
+        System.out.println("Photos     :");
+        showPhotoPaths(photos);
+        System.out.printf("Comparisons made for condition \"%s\": %d\n", album3.getCondition(), album3.getNbComps());
+
+
+        // Delete one photo and test result
+        System.out.println("\nDelete the photo ’bear.jpg’:");
+        manager.deletePhoto("bear.jpg");
+
+//      Print remaining photos
+        System.out.println("\nRemaining photos after deletion:");
+        showPhotoPaths(manager.getPhotos());
+
+        
+        // === Album 3 After Deletion ===
+        System.out.println("\n\n[ Album 3 After Deletion ]");
+        System.out.println("Name       : " + album3.getName());
+        System.out.println("Condition  : " + album3.getCondition());
+        //You can get the list of photos in album2 by calling album2.getPhotos().
+        photos = album3.getPhotos();
+        //You can write a method that prints the list of photos in album2.
+        System.out.println(album3.getCondition());
+        System.out.println("Photos     :");
+        showPhotoPaths(photos);
+        System.out.printf("Comparisons made for condition \"%s\": %d\n", album3.getCondition(), album3.getNbComps());
+
 
 }
 
